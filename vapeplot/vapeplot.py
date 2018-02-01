dd8c6ea9f9aeda51692b68950c90fcd0b49be14b
Aidan O'Brien <aidan.obrien@saberastro.com>
2018-02-01 18:04:57 +1100

    Update to PEP8 standard

--- vapeplot/vapeplot.py
+++ vapeplot/vapeplot.py
@@ -2,30 +2,39 @@
 from matplotlib import rcParams
 import matplotlib.pyplot as plt 
 import cycler,json,matplotlib,os
-loc=os.path.dirname(os.path.realpath(__file__))
+
+loc = os.path.dirname(os.path.realpath(__file__))
 pal_path = os.path.join(loc,'aesthetics.json')
 palettes = json.load(open(pal_path))
+
+
 def available(show=True):
     if not show:
         return palettes.keys()
     else:
-        f,ax = plt.subplots(4,2,figsize=(5,8))
+        f, ax = plt.subplots(4,2,figsize=(5,8))
         for i, name in enumerate(palettes.keys()):
-            x,y = i //2, i %2
+            x, y = i //2, i % 2
             cycle = palettes[name]
-            for j,c in enumerate(cycle):
-                ax[x,y].hlines(j,0,1,colors=c,linewidth=15)
-            ax[x,y].set_ylim(-1,len(cycle))
-            ax[x,y].set_title(name)
-            despine(ax[x,y],True)
+            for j, c in enumerate(cycle):
+                ax[x, y].hlines(j, 0, 1, colors=c, linewidth=15)
+            ax[x, y].set_ylim(-1, len(cycle))
+            ax[x, y].set_title(name)
+            despine(ax[x, y], True)
         plt.show()
+
+
 def check_key(palname):
     try: palettes[palname]
     except KeyError:
         raise KeyError("{} not an accepted palette name. Check vapeplot.available() for available palettes".format(palname))
+
+
 def cmap(palname):
     check_key(palname)
     return matplotlib.colors.ListedColormap(palettes[palname])
+
+
 def despine(ax,all=False):
     if all==True:
         for sp in ax.spines:
@@ -37,19 +46,29 @@ def despine(ax,all=False):
         ax.spines['right'].set_visible(False)
         ax.get_xaxis().tick_bottom()
         ax.get_yaxis().tick_left()
+
+
 def font_size(s): matplotlib.rcParams.update({'font.size': s})
+
+
 def palette(palname=None):
     if palname==None:
         return palettes
     else:
         check_key(palname)
         return palettes[palname]
+
+
 def reverse(palname):
     check_key(palname)
     return list(reversed(palette(palname)))
+
+
 def set_palette(palname):
         check_key(palname)
         rcParams['axes.prop_cycle'] = cycler.cycler(color=palettes[palname])
+
+
 def view_palette(*args):
     if len(args) > 1:
         f, ax = plt.subplots(1,len(args),figsize=(3*len(args),3))
